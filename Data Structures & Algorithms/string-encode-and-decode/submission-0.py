5bf27b525979c9e06cf7ce169ee492a7e6569aa5
Younus-Saberi <younussaberi@gmail.com>
2026-04-17 15:12:21 +0530

    Bulk sync: 17 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encode=''
+        for s in strs:
+            encode+=str(len(s))+'#'+s
+        return encode
+    def decode(self, s: str) -> List[str]:
+        decode=[]
+        i=0
+        
+        while i<len(s):
+            j=i
+            while s[j]!='#':
+                j+=1
+            length = int(s[i:j])
+            i=j+1
+            j=i+length
+            decode.append(s[i:j])
+            i=j
+
+        return decode
---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encode=''
+        for s in strs:
+            encode+=str(len(s))+'#'+s
+        return encode
+        ['4#neet4#code2#is3#good']
+
+    def decode(self, s: str) -> List[str]:
+        decode=[]
+        i=0
+        while i<len(s):
+            j=i
+            while s[j]!='#':
+                j+=1
+            length=int(s[i:j])
+            i=j+1
+            j=length+i
+            decode.append(s[i:j])
+            i=j
+
+        return decode
